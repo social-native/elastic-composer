7c083e9b42d11ddeaaafc051ea83173f2ca96cd9
Mark Rieth <markr@socialnative.com>
2021-07-09 18:18:47 -0700

    fix: update types to include nested

--- src/types.ts
+++ src/types.ts
@@ -100,6 +100,7 @@ export interface IClient<Source extends object = object> {
  */
 
 export type ESMappingType =
+    | 'nested'
     | 'long'
     | 'double'
     | 'integer'
@@ -560,7 +561,7 @@ export type ESMappingPropertyType = {
     type: ESMappingType;
 };
 export type ESMappingProperties = {
-    [field: string]: ESMappingPropertyType | {properties: ESMappingProperties};
+    [field: string]: ESMappingPropertyType | {properties: ESMappingProperties} | { type: 'nested', properties: ESMappingProperties}
 };
 
 export type ESMapping<Alias extends string> = {
