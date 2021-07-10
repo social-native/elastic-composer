d2acec87281f1c26ae0ea2bb5f347ff6fdca3a83
Mark Rieth <markr@socialnative.com>
2021-07-09 17:48:40 -0700

    feat: create failing test for flattenMappings function

--- src/__tests__/mapping_parser.test.ts
+++ src/__tests__/mapping_parser.test.ts
@@ -29,8 +29,52 @@ test('it should work when mappings have a "dynamic" field', () => {
 });
 
 test('it should work when mappings is empty', () => {
-    const fakeMapping = { 'creator-crm-testing': { mappings: {} } };
+    const fakeMapping = {'creator-crm-testing': {mappings: {}}};
     const actual = MappingParser.flattenMappings710(fakeMapping);
-    expect(actual).toEqual({})
+    expect(actual).toEqual({});
+});
 
-})
+test('it should have subfields of a nested field', () => {
+    const fakeMapping = {
+        'creator-crm-v2': {
+            mappings: {
+                dynamic: 'false',
+                properties: {
+                    key_value_tags: {
+                        type: 'nested',
+                        properties: {
+                            tag_id: {
+                                type: 'long'
+                            },
+                            tag_name: {
+                                type: 'text',
+                                fields: {
+                                    keyword: {
+                                        type: 'keyword'
+                                    }
+                                }
+                            },
+                            tag_value: {
+                                type: 'text',
+                                fields: {
+                                    keyword: {
+                                        type: 'keyword'
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+    const actualFlattenedMappings = MappingParser.flattenMappings710(fakeMapping);
+    const expectedFlattenedMapping = {
+        key_value_tags: 'nested',
+        'key_value_tags.tag_id': 'long',
+        'key_value_tags.tag_name': 'text',
+        'key_value_tags.tag_value': 'text',
+    }
+    expect(actualFlattenedMappings).toMatchObject(expectedFlattenedMapping)
+
+});
